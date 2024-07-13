b6bd4823fa8e76555b5c9e3f1428803ca4e66569
blakerob <blakerobinson@usf.edu>
2024-07-12 22:32:37 -0400

    Tasks

---
+++ tasks.js
@@ -0,0 +1,10 @@
+// U92725213
+
+const tasks = [
+    { id: 1, name: "Complete the React challenge", isCompleted: false },
+    { id: 2, name: "Review project documentation", isCompleted: false },
+    { id: 3, name: "Team meeting at 3 PM", isCompleted: false },
+  ];
+  
+  export default tasks;
+  
